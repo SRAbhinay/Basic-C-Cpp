a674fe9fa743a777f4a00cd2ad33639f495a66af
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-06 12:02:11 +0530

    Update 9-3-22 .c

--- 9-3-22 .c
+++ 9-3-22 .c
@@ -1,18 +1,18 @@
 #include <stdio.h>    
 int main()    
-{      
-    int l;
+{   int l;
     scanf("%d", &l);
     int arr1[l];
-        for (int i = 0; i <= l; i++) {     
-        scanf("%d", &arr1[i]);
-    }
+        for (int i = 0; i <= l; i++) { scanf("%d", &arr1[i]); }
+ 
     int length = sizeof(arr1)/sizeof(arr1[0]);    
     int arr2[length];      
     for (int i = 0; i < length; i++) {     
         arr2[i] = arr1[i];     
     }  
+ 
     printf("\n");
+ 
     for (int i = 0; i < length; i++) {     
         printf("%d\n", arr2[i]);    
     }    
