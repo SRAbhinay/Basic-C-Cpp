6b0aa2e0ab4ecc71c07132ed69b222907bcba8aa
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-19 11:55:44 +0530

    Update basic addition.c

--- basic addition.c
+++ basic addition.c
@@ -5,7 +5,6 @@
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
-    // calculating sum
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
